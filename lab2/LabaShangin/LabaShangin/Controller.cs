cf0a5bbb75721ed54fc81dc705d4266336eece97
EgorShangin <satar160@gmail.com>
2017-05-26 23:46:46 +0500

    Add files via upload

---
+++ lab2/LabaShangin/LabaShangin/Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaShangin
+{
+    class Controller
+    {
+        string TextBox;
+	    string Path;
+        public void writer(string FileDirectory, string Text)
+        {
+            TextBox = Text;
+            Path = FileDirectory;
+            var w = new Model();
+            w.write(Path, Text);
+        }
+        public string reader(string FileDirectory)
+	{
+	Path = FileDirectory;
+	var r = new Model();
+	TextBox = r.read(Path);
+	return TextBox;
+	}
+}
+}
---
+++ lab2/LabaShangin/LabaShangin/Model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaShangin
+{
+    class Model
+    {
+    public string read(string path)
+{
+            var s = System.IO.File.ReadAllText(path, Encoding.UTF8);
+            return s;
+}
+    public void write(string path, string Text)
+    {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(path))
+            {
+                file.Write(Text, Encoding.UTF8);
+            }
+        }
+}
+}
